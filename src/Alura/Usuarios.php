fb10b826002b98fa3fcf5279c3a59db2de653929
Elvio Amaral <elvioamaral.ea@gmail.com>
2022-05-08 20:51:16 -0300

    Aula 3 - Implementando senha.

--- src/Alura/Usuarios.php
+++ src/Alura/Usuarios.php
@@ -6,23 +6,26 @@ class Usuarios
 {
     private $nome;
     private $sobrenome;
+    private $senha;
 
-    public function __construct(string $nome)
+    public function __construct(string $nome, string $senha)
     {
         $nomeSobrenome = explode(" ", $nome, 2);
+
+        $this->senha = $senha;
+        $this->validaSenha($senha);
 
         if ($nomeSobrenome[0] === "") {
             $this->nome = 'Nome Inválido';
         } else {
             $this->nome = $nomeSobrenome[0];
         }
-        
+
         if ($nomeSobrenome[1] === null) {
             $this->sobrenome = 'Sobrenome Inválido';
         } else {
-            $this->sobrenome = $nomeSobrenome[1];            
+            $this->sobrenome = $nomeSobrenome[1];
         }
-        
     }
 
     public function getNome(): string
@@ -33,5 +36,19 @@ class Usuarios
     public function getSobrenome(): string
     {
         return $this->sobrenome;
+    }
+    
+    public function getSenha(): string
+    {
+        return $this->senha;
+    }
+
+    private function validaSenha(string $senha): void
+    {
+        $size = strlen($senha);
+
+        if ($size < 6) {
+            $this->senha = "Senha inválida.";
+        }
     }
 }
