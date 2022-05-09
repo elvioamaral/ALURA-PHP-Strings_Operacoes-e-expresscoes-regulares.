ecdfbe4f2a2cb0b2ae192bd640f6df455d2fe92d
Elvio Amaral <elvioamaral.ea@gmail.com>
2022-05-08 23:04:16 -0300

    Aula 5 - Substituição com expressões regulares.

--- cadastro.php
+++ cadastro.php
@@ -5,7 +5,7 @@
 
 require 'autoload.php';
 
-$usuario = new App\Alura\Usuarios($_POST['nome'], $_POST['senha']);
+$usuario = new App\Alura\Usuarios($_POST['nome'], $_POST['senha'], $_POST['genero']);
 $contato = new App\Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['cep'], $_POST['telefone']);
 ?>
 
@@ -23,7 +23,7 @@ $contato = new App\Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['ce
 
 <div class="mx-5 my-5">
 <h1>Cadastro feito com sucesso.</h1>
-<p>Seguem os dados de sua conta:</p>
+<p><?php echo $usuario->getTratamento(); ?>, seguem os dados de sua conta:</p>
 <ul class="list-group">
     <li class="list-group-item">Primeiro nome: <?php echo $usuario->getNome(); ?> </li>
     <li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome(); ?> </li>
